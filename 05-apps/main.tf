87eee60b7725e888928dc4699b83c787f9215aa7
samarareddy <samara.junna@gmail.com>
2025-04-14 09:35:52 +0530

    dev

--- 05-apps/main.tf
+++ 05-apps/main.tf
@@ -51,7 +51,7 @@ module "ansible" {
       Name = "${var.project_name}-${var.environment}-ansible"
     }
   )
-  depends_on = [ module.backend, module.frontend]
+  depends_on = [ module.backend,module.frontend]
 }
 
 module "records" {
